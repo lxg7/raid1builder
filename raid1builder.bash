d1c4d96669f17e4df3959975d2102adebecf2a63
Alexandro___ <alex5356622@yandex.ru>
2022-06-14 14:33:08 +0300

    makeraid def_disk_in_use

--- raid1builder.bash
+++ raid1builder.bash
@@ -6,13 +6,30 @@ E_FILE_NOT_EXIST=71
 E_NOFILE=66
 
 function makeraid0 {
-    echo -n "Выбран RAID-0."
-    echo -n "В данный момент не поддерживается..."
+    echo  "Выбран RAID-0."
+    echo  "В данный момент не поддерживается..."
+    echo  -n "Будет ли текущий диск частью будущего массива?(y/n) "
+    read def_disk_in_root
+    if def_disk_in_root == 'y'
+    then
+      echo -n "Текущий диск будет вхоить в систему..."
+    else
+      echo -n "Текущий диск не будет вхоить в систему..."
+    fi
+
 }
 
 function makeraid1 {
-    echo -n "Выбран RAID-1."
-    echo -n "В данный момент не поддерживается..."
+    echo  "Выбран RAID-1."
+    echo  "В данный момент не поддерживается..."
+    echo  -n "Будет ли текущий диск частью будущего массива?(y/n) "
+    read def_disk_in_root
+    if def_disk_in_root == 'y'
+    then
+      echo -n "Текущий диск будет вхоить в систему..."
+    else
+      echo -n "Текущий диск не будет вхоить в систему..."
+    fi
 }
 
 
@@ -22,13 +39,16 @@ then
 	echo "Для работы сценария требуются права root."
 	exit $E_NOTROOT
 fi
+
+echo "Root - права получены"
+
+
  # Проверка наличия необходимых программ
 command -v mdadm >/dev/null 2>&1 || { echo >&2 "I require mdadm but it's not installed.  Aborting."; exit 1; }
 command -v parted >/dev/null 2>&1 || { echo >&2 "I require parted but it's not installed.  Aborting."; exit 1; }
 command -v rsync >/dev/null 2>&1 || { echo >&2 "I require rsync but it's not installed.  Aborting."; exit 1; }
 # command -v foo >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
 
-echo "Root - права получены"
 echo "Все программы установлены (mdadm, parted, rsync)"
 
 echo -n "Enter RAID level(1,2): "
@@ -36,7 +56,7 @@ read rraidlevel
 
 echo -n "Выбран $raidlevel : "
 
-case $COUNTRY in
+case $rraidlevel in
 
   RAID0 | raid0 | 0)
     makeraid0
