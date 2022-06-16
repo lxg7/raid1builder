dff74bfdf6e82e2436df7c0db2abd86236da284d
Alex Galchev <alex5356622@yandex.ru>
2022-06-16 22:47:48 +0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-
+update-grub
+grub-install $1
+grub-install $2
 update-initramfs -u
 sed '8i\GRUB_RECORDFAIL_TIMEOUT=10' -i /etc/default/grub
 sed "s/quiet/quiet bootdegraded/" -i /etc/default/grub
-update-grub
-dpkg-reconfigure -p critical mdadm
+
+#dpkg-reconfigure -p critical mdadm
